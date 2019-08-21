37491fbd65ac3282c61358155751a60f1a3bcd2e
bdlondon <34342474+bdlondon@users.noreply.github.com>
2019-08-21 11:50:03 -0700

    Update zdog-demo.js

--- zdog-demo.js
+++ zdog-demo.js
@@ -1,15 +1,5 @@
 // Made with Zdog.  Original design by Alex Pasquarella
 
-Zdog.Anchor.prototype.renderGraphSvg = function( svg ) {
-  if ( !svg ) {
-    throw new Error( 'svg is ' + svg + '. ' +
-      'SVG required for render. Check .renderGraphSvg( svg ).' );
-  }
-  this.flatGraph.forEach( function( item ) {
-    item.render( svg, Zdog.SvgRenderer );
-  });
-};
-
 // colors
 var red = '#F44';
 var navy = '#247';
